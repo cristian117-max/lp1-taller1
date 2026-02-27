01567f79377cb85407318ad0f4b8ccd7cba36b6e
cristian117-max <cristianibanez1422@gmail.com>
2026-02-26 19:35:15 -0500

    solucion problema 8

--- problema8/main.go
+++ problema8/main.go
@@ -2,32 +2,68 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 // Objetivo: Simular "futuros" en Go usando canales. Una función lanza trabajo asíncrono
 // y retorna un canal de solo lectura con el resultado futuro.
-// TODO: completa las funciones y experimenta con varios futuros a la vez.
+//  completa las funciones y experimenta con varios futuros a la vez.
 
 func asyncCuadrado(x int) <-chan int {
 	ch := make(chan int)
 	go func() {
 		defer close(ch)
-		// TODO: simular trabajo
-
+		//  simular trabajo
+		time.Sleep(500 * time.Microsecond)
 		ch <- x * x
 	}()
 	return ch
 }
 
 func main() {
-	// TODO: crea varios futuros y recolecta sus resultados: f1, f2, f3
+	//  crea varios futuros y recolecta sus resultados: f1, f2, f3
+	f1 := asyncCuadrado(2)
+	f2 := asyncCuadrado(3)
+	f3 := asyncCuadrado(4)
 
-	// TODO: Opción 1: esperar cada futuro secuencialmente
+	//  Opción 1: esperar cada futuro secuencialmente
+	fmt.Println("secuencial:")
+	fmt.Println(<-f1)
+	fmt.Println(<-f2)
+	fmt.Println(<-f3)
 
-	
-	// TODO: Opción 2: fan-in (combinar múltiples canales)
+	//  Opción 2: fan-in (combinar múltiples canales)
 	// Pista: crea una función fanIn que recibe múltiples <-chan int y retorna un único <-chan int
 	// que emita todos los valores. Requiere goroutines y cerrar el canal de salida cuando todas terminen.
-	
+
+	fanIn := func(channels ...<-chan int) <-chan int {
+		out := make(chan int)
+		var wg sync.WaitGroup
+		wg.Add(len(channels))
+
+		for _, c := range channels {
+			go func(c <-chan int) {
+				defer wg.Done()
+				for v := range c {
+					out <- v
+				}
+			}(c)
+		}
+
+		go func() {
+			wg.Wait()
+			close(out)
+		}()
+
+		return out
+	}
+
+	fmt.Println("fan-in:")
+	f4 := asyncCuadrado(5)
+	f5 := asyncCuadrado(6)
+	f6 := asyncCuadrado(7)
+	for v := range fanIn(f4, f5, f6) {
+		fmt.Println(v)
+	}
 }
